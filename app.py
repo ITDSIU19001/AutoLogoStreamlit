393826edd5849040a2c1925f7b48e3d772962881
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2024-01-31 15:57:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class WatermarkApp:
                 watermarked_image = self.add_watermark_to_image(uploaded_file, watermark_path, watermark_position, watermark_size, opacity, max_dimension_percent)
                 watermarked_images.append(watermarked_image)
 
-                st.image(watermarked_image, caption=f"Watermarked Image {i}/{n_files}")
+                st.image(watermarked_image.thumbnail((100, 100)), caption=f"Watermarked Image {i}/{n_files}")
 
             return watermarked_images
 
@@ -134,7 +134,7 @@ def main():
     with st.expander("Danh sách ảnh đã được đóng dấu"):
         if watermarked_images:
             for i, image in enumerate(watermarked_images, start=1):
-                st.image(image, caption=f"Watermarked Image {i}")
+                st.image(image.thumbnail((100, 100)), caption=f"Watermarked Image {i}")
 
     if watermarked_images:
         # Zip watermarked images
