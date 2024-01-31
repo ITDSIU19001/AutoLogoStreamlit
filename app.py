0f1e46c4c68dc9366fcf5c13bd2441f4c654c87b
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2024-01-31 16:13:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,56 +20,59 @@ class WatermarkApp:
         )
 
     @st.cache
-    def add_watermark_to_image(self, uploaded_file, watermark_path, position="Bottom Right", size=50, opacity=0.2, max_dimension_percent=50):
-        original_image = Image.open(uploaded_file)
+    def add_watermark_to_image(self, uploaded_files, watermark_path, position="Bottom Right", size=50, opacity=0.2, max_dimension_percent=50):
+        watermarked_images = []
+        for uploaded_file in uploaded_files:
+            original_image = Image.open(uploaded_file)
+            # Convert to RGB mode if the image is in CMYK mode
+            if original_image.mode == "CMYK":
+                original_image = original_image.convert("RGB")
 
-        # Convert to RGB mode if the image is in CMYK mode
-        if original_image.mode == "CMYK":
-            original_image = original_image.convert("RGB")
+            # Calculate the maximum dimensions based on the original image size and the specified percentage
+            max_width = int(original_image.width * max_dimension_percent / 100)
+            max_height = int(original_image.height * max_dimension_percent / 100)
 
-        # Calculate the maximum dimensions based on the original image size and the specified percentage
-        max_width = int(original_image.width * max_dimension_percent / 100)
-        max_height = int(original_image.height * max_dimension_percent / 100)
+            # Resize the image to reduce processing time
+            original_image.thumbnail((max_width, max_height))
 
-        # Resize the image to reduce processing time
-        original_image.thumbnail((max_width, max_height))
+            watermark = Image.open(watermark_path)
 
-        watermark = Image.open(watermark_path)
+            # Resize watermark based on size percentage
+            watermark_width = original_image.width * size // 100
+            w_percent = watermark_width / float(watermark.width)
+            watermark_height = int(w_percent * watermark.height)
+            watermark = watermark.resize((watermark_width, watermark_height), Image.LANCZOS)
 
-        # Resize watermark based on size percentage
-        watermark_width = original_image.width * size // 100
-        w_percent = watermark_width / float(watermark.width)
-        watermark_height = int(w_percent * watermark.height)
-        watermark = watermark.resize((watermark_width, watermark_height), Image.LANCZOS)
+            # Calculate watermark position
+            if "Trên" in position:
+                y_position = 0
+            elif "Dưới" in position:
+                y_position = original_image.height - watermark_height
+            else:
+                y_position = (original_image.height - watermark_height) // 2
 
-        # Calculate watermark position
-        if "Trên" in position:
-            y_position = 0
-        elif "Dưới" in position:
-            y_position = original_image.height - watermark_height
-        else:
-            y_position = (original_image.height - watermark_height) // 2
+            if "Trái" in position:
+                x_position = 0
+            elif "Phải" in position:
+                x_position = original_image.width - watermark_width
+            else:
+                x_position = (original_image.width - watermark_width) // 2
 
-        if "Trái" in position:
-            x_position = 0
-        elif "Phải" in position:
-            x_position = original_image.width - watermark_width
-        else:
-            x_position = (original_image.width - watermark_width) // 2
+            # Convert opacity to alpha value
+            watermark = watermark.convert("RGBA")
+            watermark_with_opacity = Image.new("RGBA", watermark.size)
+            for x in range(watermark.width):
+                for y in range(watermark.height):
+                    r, g, b, a = watermark.getpixel((x, y))
+                    watermark_with_opacity.putpixel((x, y), (r, g, b, int(a * opacity)))
 
-        # Convert opacity to alpha value
-        watermark = watermark.convert("RGBA")
-        watermark_with_opacity = Image.new("RGBA", watermark.size)
-        for x in range(watermark.width):
-            for y in range(watermark.height):
-                r, g, b, a = watermark.getpixel((x, y))
-                watermark_with_opacity.putpixel((x, y), (r, g, b, int(a * opacity)))
+            # Paste watermark onto original image
+            watermarked_image = original_image.copy()
+            watermarked_image.paste(watermark_with_opacity, (x_position, y_position), watermark_with_opacity)
 
-        # Paste watermark onto original image
-        watermarked_image = original_image.copy()
-        watermarked_image.paste(watermark_with_opacity, (x_position, y_position), watermark_with_opacity)
+            watermarked_images.append(watermarked_image)
 
-        return watermarked_image
+        return watermarked_images
 
     def image_to_bytes(self, image):
         img_byte_array = io.BytesIO()
