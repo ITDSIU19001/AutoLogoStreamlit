3026d445b56e675ab260bfa0970d2155b6edeb40
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2024-02-01 20:37:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 import os
 import io
 import zipfile
 from base64 import b64encode
-import pandas as pd
 
 class WatermarkApp:
     def __init__(self):
@@ -41,9 +41,17 @@ class WatermarkApp:
                         progress_bar.progress(i / n_files)
                         counter_text.text(f"{i}/{n_files} images watermarked")
 
+                        # Update CSV file
+                        self.update_csv(i, n_files)
+
                 # Provide download button for the zip file
                 st.download_button(label="Download Watermarked Images", data=output_zip.getvalue(), file_name="watermarked_images.zip")
-                self.update_csv(len(uploaded_files))
+
+    def update_csv(self, index, total_images):
+        df = pd.read_csv("data.csv")
+        df.loc[index-1, "Status"] = "Processing"
+        df.loc[index-1, "Amount"] = total_images
+        df.to_csv("data.csv", index=False)
 
     def preview_watermark(self, uploaded_files, watermark_path, watermark_position, watermark_size, opacity, max_dimension_percent):
         if uploaded_files and watermark_path:
@@ -124,21 +132,6 @@ class WatermarkApp:
         img_byte_array = io.BytesIO()
         image.save(img_byte_array, format="PNG")
         return img_byte_array
-    def update_csv(self, num_images_processed):
-        # Load CSV data
-        df = pd.read_csv('data.csv')
-
-        # Update index and amount columns
-        df['index'] += 1
-        df['amount'] = num_images_processed
-
-        # Update download column if user clicks download
-        # Assuming there's a global variable or some other mechanism to track download clicks
-        # For example, if download_clicked is True when the user clicks download
-        # df['download'] = 'X' if download_clicked else ''
-
-        # Save updated data to CSV
-        df.to_csv('data.csv', index=False)
 
 def main():
     app = WatermarkApp()
